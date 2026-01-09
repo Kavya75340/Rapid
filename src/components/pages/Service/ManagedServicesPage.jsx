b49152099eb3a4ccd6d0dfd6196d04f522cf6a1f
Kavya75340 <kvvarshney7534@gmail.com>
2026-01-09 11:13:08 +0530

    Vercel

--- src/components/pages/Service/ManagedServicesPage.jsx
+++ src/components/pages/Service/ManagedServicesPage.jsx
@@ -1,16 +1,16 @@
 import { managedServicesPage } from "./pages/managed-s";
-import HeroSection from "../../sections/service/HeroSection";
-import WhatWeDoSection from "../../sections/service/WhatWeDoSection";
-import HowWeHelpSection from "../../sections/service/HowWeHelpSection";
-import FAQSection from "../../sections/service/FAQSection";
-import JumpToNav from "../../sections/service/JumpToNav";
-import MarqueeBanner from "../../sections/service/MarqueeBanner";
-import AwardsSection from "../../sections/service/AwardsSection";
-import AIAssessmentBanner from "../../sections/service/AIAssessmentBanner";
-import InsightsSection from "../../sections/service/InsightsSection";
-import CustomersSection from "../../sections/service/CustomersSection";
-import ExpertVoicesSection from "../../sections/service/ExpertVoicesSection";
-import PartnersSection from "../../sections/service/PartnersSection";
+import HeroSection from "@/sections/service/HeroSection";
+import WhatWeDoSection from "@/sections/service/WhatWeDoSection";
+import HowWeHelpSection from "@/sections/service/HowWeHelpSection";
+import FAQSection from "@/sections/service/FAQSection";
+import JumpToNav from "@/sections/service/JumpToNav";
+import MarqueeBanner from "@/sections/service/MarqueeBanner";
+import AwardsSection from "@/sections/service/AwardsSection";
+import AIAssessmentBanner from "@/sections/service/AIAssessmentBanner";
+import InsightsSection from "@/sections/service/InsightsSection";
+import CustomersSection from "@/sections/service/CustomersSection";
+import ExpertVoicesSection from "@/sections/service/ExpertVoicesSection";
+import PartnersSection from "@/sections/service/PartnersSection";
 const ManagedServicesPage = () => {
     return (
         <>
