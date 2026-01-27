4424f99d2c8bf810504e8036318be3dc4d97f33b
Kavya75340 <kvvarshney7534@gmail.com>
2026-01-27 16:22:06 +0530

    change hightech

---
+++ src/components/pages/Industries/HighTech.jsx
@@ -0,0 +1,24 @@
+import { hightech } from "./data/hightech.js";
+import CTASection from "./common/CTASection";
+import ExpertsSection from "./section/ExpertsSection";
+import HeroSection from "./section/HeroSection";
+import MarqueeSection from "../CommonSection/MarqueeSection.jsx";
+import PerspectivesSection from "./section/PerspectivesSection";
+import ResourcesSection from "./section/ResourcesSection";
+import StatsSection from "./common/StatsSection";
+
+const HighTech = () => {
+    return (
+        <>
+            <HeroSection data={highTech.heroAccordion} />
+            <MarqueeSection />
+            <StatsSection />
+            <ExpertsSection data={highTech.expertsQA} />
+            <PerspectivesSection data={highTech.perspectives} />
+            <ResourcesSection data={highTech.resources} />
+            <CTASection />
+            <MarqueeSection />
+        </>
+    );
+};
+export default HighTech;
